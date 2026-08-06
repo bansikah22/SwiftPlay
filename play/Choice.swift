2e9e9e0f508c539b958410b8f11b1c16ab821955
Noel Bansikah <tandapnoelbansikah@gmail.com>
2026-08-06 15:05:07 +0100

    Create Choice.swift

---
+++ play/Choice.swift
@@ -0,0 +1,17 @@
+// Noel Bansikah 06/08/2026
+
+import Foundation
+
+let age = 20
+let temperature = 30
+let score = 75
+
+let ageStatus = age >= 18 ? "Adult" : "Minor"
+
+let weather = temperature > 25 ? "Hot" : "Cold"
+
+let passed = score >= 50 ? "Pass" : "Fail"
+
+print("Age Status : \(ageStatus)")
+print("Weather    : \(weather)")
+print("Result     : \(passed)")
