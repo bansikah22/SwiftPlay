c68fabedb3be0a1036333295db6ef4885a0305b9
Noel Bansikah <tandapnoelbansikah@gmail.com>
2026-08-07 23:28:21 +0100

    update add 2D array

--- play/Check.swift
+++ play/Check.swift
@@ -15,3 +15,16 @@ let passed = score >= 50 ? "Pass" : "Fail"
 print("Age Status : \(ageStatus)")
 print("Weather    : \(weather)")
 print("Result     : \(passed)")
+
+// 07/08/2026
+// 2D arrays
+
+var grid = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+print(grid[0][1]) // 2
+for row in grid {
+  print(row)
+}
